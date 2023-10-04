557283d61c111c6faa3f53462aec27164f29baf0
dmytroklymchuk <dmytro.klymchuk@valtech.com>
2023-10-04 12:02:43 +0300

    Using POST request

--- src/App.js
+++ src/App.js
@@ -1,55 +1,93 @@
-import React , {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
 
-
-    const fetchMoviesHandler = useCallback( async () => {
+    const fetchMoviesHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            const response = await fetch('https://react-http-23698-default-rtdb.firebaseio.com/movies.json');
             if (!response.ok) {
-              throw new Error('Something went wrong!!')
+                throw new Error('Something went wrong!');
             }
+
             const data = await response.json();
 
-            const transformedMovies = data.results.map(movieData => {
-                return {
-                  id: movieData.episode_id,
-                  title: movieData.title,
-                  openingText: movieData.opening_crawl,
-                  releaseDate: movieData.release_date
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate
+                })
+            }
+
+            // const transformedMovies = loadedMovies.map((movieData) => {
+            //     return {
+            //         id: movieData.episode_id,
+            //         title: movieData.title,
+            //         openingText: movieData.openingText,
+            //         releaseDate: movieData.releaseDate,
+            //     };
+            // });
+            setMovies(loadedMovies);
         } catch (error) {
-            setError(error.message)
+            setError(error.message);
         }
         setIsLoading(false);
-  },[])
+    }, []);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetchMoviesHandler();
-    }, [fetchMoviesHandler]); // Side effect, avoid looping render, add deps
+    }, [fetchMoviesHandler]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-          {!isLoading && <MoviesList movies={movies} />}
-          {isLoading && <p>Loading.....</p>}
-          {!isLoading && error && <p>{error}</p>}
-      </section>
-    </React.Fragment>
-  );
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://react-http-23698-default-rtdb.firebaseio.com/movies.json',{
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data)
+    }
+
+    let content = <p>Found no movies.</p>;
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+    }
+
+    if (error) {
+        content = <p>{error}</p>;
+    }
+
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </React.Fragment>
+    );
 }
 
 export default App;
